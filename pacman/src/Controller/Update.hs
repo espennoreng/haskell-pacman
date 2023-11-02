2f8f19ca819c6ca96a9c38600fdf2a553bca9af3
Espen Noreng <espennoreng@gmail.com>
2023-11-02 20:58:39 +0100

    Added some comments

--- pacman/src/Controller/Update.hs
+++ pacman/src/Controller/Update.hs
@@ -1,34 +1,57 @@
 module Controller.Update where
 
-import Model.GameState.Types
-import Model.GameState.Functions
-import Model.Pacman.Functions
-import Model.Ghosts.Functions
-import Model.Board.GameBoard
 import Controller.HighScore
-
+import Model.Board.GameBoard
+import Model.GameState.Functions
+import Model.GameState.Types
+import Model.Ghosts.Functions
+import Model.Pacman.Functions
 
 handleUpdate :: Float -> GameState -> IO GameState
 handleUpdate deltaTime gameState@(GameState pacman food ghosts gen score lives screen paused) =
-  if paused || screen /= GameScreen then
-    return gameState
-  else do
-    let updatedPacman = movePacman pacmanGameBoard pacman
-        updatedGhosts = map (updateGhostTimer deltaTime) ghosts
-        releasedGhosts = map (releaseGhostIfNeeded pacmanGameBoard) updatedGhosts
-        (movedGhosts, newGen) = moveGhosts gen pacmanGameBoard updatedPacman releasedGhosts
-        (foodEaten, newFood) = pacmanEatsFood updatedPacman food
-        scoreIncrement = if foodEaten then 1 else 0
-        newScore = score + scoreIncrement
-        newLives = pacmanLoosesLife updatedPacman movedGhosts lives
-        newGameScreen = if newLives == 0 then GameOverScreen else GameScreen
+  if paused || screen /= GameScreen
+    then return gameState
+    else do
+      -- Update the positions and check for collisions.
+      let updatedPacman = movePacman pacmanGameBoard pacman
+          updatedGhosts = map (updateGhostTimer deltaTime) ghosts
+          releasedGhosts = map (releaseGhostIfNeeded pacmanGameBoard) updatedGhosts
+          (movedGhosts, newGen) = moveGhosts gen pacmanGameBoard updatedPacman releasedGhosts
+          (foodEaten, newFood) = pacmanEatsFood updatedPacman food
+          scoreIncrement = if foodEaten then 1 else 0
 
-    -- Handling score and high score
-    if newLives == 0 then
-      do writeHighScore newScore
-         return $ restartGame gameState { score = newScore }
-    else if newLives < lives then
-      return $ resetPositionsAndLooseLife gameState { score = newScore, food = newFood, screen = newGameScreen, randGen = newGen, lives = newLives }
-    else
-      return $ gameState { pacman = updatedPacman, ghosts = movedGhosts, food = newFood, score = newScore, screen = newGameScreen, randGen = newGen }
-   
+      -- Calculate the new state after collisions.
+      let newLives = pacmanLoosesLife updatedPacman movedGhosts lives
+          newScore = score + scoreIncrement
+          newGameScreen = if newLives == 0 then GameOverScreen else GameScreen
+
+      -- Check if Pacman died to either reset or continue the game.
+      if newLives < lives
+        then do
+          -- Pacman lost a life, reset positions and update the state.
+          if newLives > 0
+            then
+              return $
+                resetPositionsAndLooseLife
+                  gameState
+                    { score = newScore,
+                      food = newFood,
+                      screen = newGameScreen,
+                      randGen = newGen,
+                      lives = newLives
+                    }
+            else do
+              -- Pacman has no more lives, game over.
+              writeHighScore newScore
+              return $ restartGame gameState {score = newScore}
+        else -- No life lost, continue the game with updated state.
+
+          return $
+            gameState
+              { pacman = updatedPacman,
+                ghosts = movedGhosts,
+                food = newFood,
+                score = newScore,
+                screen = newGameScreen,
+                randGen = newGen
+              }
