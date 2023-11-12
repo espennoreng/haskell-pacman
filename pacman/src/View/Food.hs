0add9e48d845267ca88e73d54ffa6f79a687655f
AkselCan <aksel2606@gmail.com>
2023-11-12 02:59:04 +0100

    powerCookieToPicture added

--- pacman/src/View/Food.hs
+++ pacman/src/View/Food.hs
@@ -4,7 +4,10 @@ import Graphics.Gloss
 import Model.GameState.Types
 import Model.Utils.Functions
 
+
 foodToPicture :: Food -> Picture
 foodToPicture food = translatePosition food $ Color cyan $ circleSolid 2
 
+powerCookieToPicture :: Food -> Picture
+powerCookieToPicture food = translatePosition food $ Color cyan $ circleSolid 4
 
